3c37705f5f53ee29f630e1744639269d990c801c
nordluma <maxnordlund.95@hotmail.com>
2025-03-13 23:20:22 +0200

    feat: parse args and start transport server

--- contextual_backend/src/main.rs
+++ contextual_backend/src/main.rs
@@ -1,4 +1,23 @@
+use contextual_backend::{
+    args::{Args, TransportType},
+    jsonrpc::JsonRpcServer,
+    transport::{Transport, tcp::TcpTransport},
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let args = Args::parse_and_validate();
+
+    let mut server = JsonRpcServer::new();
+
+    match args.transport {
+        TransportType::Unix { socket_path } => {
+            todo!()
+        }
+        TransportType::Tcp { host, port } => {
+            let transport = TcpTransport::new(&host, port);
+            transport.start(server).await.unwrap();
+        }
+        TransportType::Stdio => todo!(),
+    }
 }
