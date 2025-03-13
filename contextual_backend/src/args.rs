6c7f4ce71dd65e874f804c8f77df0a5dc2bf1f2d
nordluma <maxnordlund.95@hotmail.com>
2025-03-13 23:17:16 +0200

    feat(args): use `derive` to parse args and add validation

--- contextual_backend/src/args.rs
+++ contextual_backend/src/args.rs
@@ -1,39 +1,111 @@
-use clap::{Arg, ArgMatches, Command};
+use clap::{Error, Parser, ValueEnum, error::ErrorKind};
 
-pub fn parse_args() -> ArgMatches {
-    Command::new("Contextual Backend")
-        .arg(
-            Arg::new("transport")
-                .short('t')
-                .long("transport")
-                .value_name("TYPE")
-                .help("Transport type (unix, tcp, stdio)")
-                .required(true)
-                .default_value("unix"),
-        )
-        .arg(
-            Arg::new("host")
-                .long("host")
-                .help("TCP host address")
-                .required(true)
-                .default_value("127.0.0.1"),
-        )
-        .arg(
-            Arg::new("port")
-                .short('p')
-                .long("port")
-                .value_name("PORT")
-                .help("TCP port")
-                .default_value("9876"),
-        )
-        .arg(
-            Arg::new("socket")
-                .short('s')
-                .long("socke")
-                .value_name("PATH")
-                .help("Unix socket path")
-                .required(true)
-                .default_value("/tmp/contextual.sock"),
-        )
-        .get_matches()
+#[derive(Parser)]
+#[command(name = "Contextual Backend")]
+pub struct Args {
+    /// Transport type (unix, tcp, stdio)
+    #[arg(
+        short = 't',
+        long,
+        value_name = "TYPE",
+        default_value_t = Transport::Stdio
+    )]
+    transport: Transport,
+
+    /// TCP host address (required when transport is tcp)
+    #[arg(long, value_name = "HOST")]
+    host: Option<String>,
+
+    /// TCP port (required when transport is tcp)
+    #[arg(short = 'p', long, value_name = "PORT")]
+    port: Option<u16>,
+
+    /// Unix socket path (required when transport is unix)
+    socket: Option<String>,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum Transport {
+    Unix,
+    Tcp,
+    Stdio,
+}
+
+impl std::fmt::Display for Transport {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let transport_type = match self {
+            Transport::Unix => "unix",
+            Transport::Tcp => "tcp",
+            Transport::Stdio => "stdio",
+        };
+        write!(f, "{}", transport_type)
+    }
+}
+
+impl Args {
+    /// Parse arguments from the command line and validate argument
+    /// combinations depending on the [TransportType].
+    pub fn parse_and_validate() -> ValidatedArgs {
+        let args = Self::parse();
+        if let Err(e) = args.validate() {
+            e.exit();
+        }
+
+        ValidatedArgs {
+            // we can safely unwrap here since the values have been validated
+            // before this
+            transport: match args.transport {
+                Transport::Unix => TransportType::Unix {
+                    socket_path: args.socket.unwrap(),
+                },
+                Transport::Tcp => TransportType::Tcp {
+                    host: args.host.unwrap(),
+                    port: args.port.unwrap(),
+                },
+                Transport::Stdio => TransportType::Stdio,
+            },
+        }
+    }
+
+    /// Validate arguments base on transport type
+    fn validate(&self) -> Result<(), clap::Error> {
+        match self.transport {
+            Transport::Unix => {
+                if self.socket.is_none() {
+                    return Err(Error::raw(
+                        ErrorKind::MissingRequiredArgument,
+                        "The argument '--socket <PATH>' is required when transport is 'unix'",
+                    ));
+                }
+            }
+            Transport::Tcp => {
+                if self.host.is_none() {
+                    return Err(Error::raw(
+                        ErrorKind::MissingRequiredArgument,
+                        "The argument --host <HOST> is required when transport is 'tcp'",
+                    ));
+                }
+
+                if self.port.is_none() {
+                    return Err(Error::raw(
+                        ErrorKind::MissingRequiredArgument,
+                        "The argument --port <PORT> is required when transport is 'tcp'",
+                    ));
+                }
+            }
+            Transport::Stdio => {}
+        }
+
+        Ok(())
+    }
+}
+
+pub struct ValidatedArgs {
+    pub transport: TransportType,
+}
+
+pub enum TransportType {
+    Unix { socket_path: String },
+    Tcp { host: String, port: u16 },
+    Stdio,
 }
