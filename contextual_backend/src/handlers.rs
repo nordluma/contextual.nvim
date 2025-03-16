edd395ee5e99de904165ddb28e0106984fd70b9a
nordluma <maxnordlund.95@hotmail.com>
2025-03-16 14:40:52 +0200

    feat(handlers): define handler to save a new note

---
+++ contextual_backend/src/handlers.rs
@@ -0,0 +1,23 @@
+use serde_json::{Value, json};
+
+use crate::database::Store;
+
+type GenError = Box<dyn std::error::Error>;
+
+pub struct Handler<DB> {
+    database: DB,
+}
+
+impl<DB: Store> Handler<DB> {
+    pub fn new(database: DB) -> Self {
+        Self { database }
+    }
+
+    pub async fn save_note(&self, params: Value) -> Result<Value, GenError> {
+        let new_note = params.try_into()?;
+        match self.database.save_note(new_note).await {
+            Ok(note_id) => Ok(json!({"id": note_id})),
+            Err(e) => Err(format!("Database error: {e:?}").into()),
+        }
+    }
+}
