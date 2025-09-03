f8c59340743c7b52815e92d508a07d5bd8665a94
nordluma <maxnordlund.95@hotmail.com>
2025-09-03 21:03:27 +0300

    feat(contextual_backend): define todo items

--- contextual_backend/src/types/mod.rs
+++ contextual_backend/src/types/mod.rs
@@ -1,3 +1,4 @@
 pub mod note;
+pub mod todo;
 
 pub use note::*;
--- contextual_backend/src/types/todo.rs
+++ contextual_backend/src/types/todo.rs
@@ -1,5 +1,7 @@
 use anyhow::Context;
+use chrono::{DateTime, Utc};
 use serde_json::Value as JsonValue;
+use uuid::Uuid;
 
 #[derive(Debug)]
 pub struct NewTodoItem {
@@ -43,5 +45,35 @@ impl TryFrom<JsonValue> for NewTodoItem {
             line_number,
             content,
         })
+    }
+}
+
+#[derive(Debug)]
+pub struct TodoItem {
+    pub id: Uuid,
+    pub hash: String,
+    pub branch: String,
+    pub file_path: String,
+    pub line_number: u64,
+    pub content: String,
+    pub created_at: chrono::DateTime<Utc>,
+    pub deleted_at: Option<DateTime<Utc>>,
+}
+
+impl TodoItem {
+    pub fn new(new_todo: NewTodoItem) -> Self {
+        let id = Uuid::new_v4();
+        let hash = String::new(); // TODO: compute hash
+
+        Self {
+            id,
+            hash,
+            branch: new_todo.branch,
+            file_path: new_todo.file_path,
+            line_number: new_todo.line_number,
+            content: new_todo.content,
+            created_at: Utc::now(),
+            deleted_at: None,
+        }
     }
 }
