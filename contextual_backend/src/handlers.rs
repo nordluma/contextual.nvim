b27d97c4233ee9b7a26a145a784ed6180ed255df
nordluma <maxnordlund.95@hotmail.com>
2025-09-16 12:51:26 +0300

    feat(contextual_backend/handlers): define handler for syncing todos

--- contextual_backend/src/handlers.rs
+++ contextual_backend/src/handlers.rs
@@ -1,6 +1,6 @@
 use serde_json::{Value, json};
 
-use crate::database::Store;
+use crate::{database::Store, types::todo::NewTodoItems};
 
 pub struct Handler<DB> {
     database: DB,
@@ -16,6 +16,13 @@ impl<DB: Store> Handler<DB> {
         let note_id = self.database.save_note(new_note).await?;
 
         Ok(json!({"id": note_id}))
+    }
+
+    pub async fn sync_todos(&self, params: Value) -> Result<Value, anyhow::Error> {
+        let _project_todos: NewTodoItems = params.try_into()?;
+        let _saved_todos = self.database.get_todos().await?;
+
+        todo!()
     }
 
     pub async fn save_todo_item(&self, params: Value) -> Result<Value, anyhow::Error> {
