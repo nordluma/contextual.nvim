db71380ec89503e86d4a286f4b6086ac17887160
nordluma <maxnordlund.95@hotmail.com>
2025-09-16 12:53:45 +0300

    feat(lua/contextual): function to parse content length from message

--- lua/contextual/jsonrpc.lua
+++ lua/contextual/jsonrpc.lua
@@ -28,7 +28,24 @@ M.NewJsonRpcRequest = function(id, method, params)
 	}
 end
 
----@param response_payload string
+local parse_header = function(response)
+	local content_length = response:match("Content%-Length:%s*(%d+)")
+	if not content_length then
+		vim.notify("No Content-Length found", vim.log.levels.WARN)
+		return nil
+	end
+
+	content_length = tonumber(content_length)
+	local header_end = response:find("\r\n\r\n", 1, true)
+	if not header_end then
+		vim.notify("No header terminator found", vim.log.levels.WARN)
+	end
+
+	local body = response:sub(header_end + 4, header_end + 3 + content_length)
+
+	return content_length, body
+end
+
 ---@return JsonRpcResponse|nil
 M.ParseJsonRpcResponse = function(response_payload)
 	return vim.json.decode(response_payload)
